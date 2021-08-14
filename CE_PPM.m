9c0907191971bd6004ea42b18026132da5e5627f
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-08-14 13:16:27 +0530

    Update CE_PPM.m

--- CE_PPM.m
+++ CE_PPM.m
@@ -1,47 +1,27 @@
 clc;
 close all;
 clear all;
-Fsampling=10;
-Fmessage=3;
-Amessage=2;
-Asaw=3;
-t=0:0.001:1; 
-saw_signal=Asaw*sawtooth(2*pi*Fsampling*t); 
-subplot(4,1,1);
-plot(t,saw_signal); 
-title('Comparator Wave');
-xlabel('Time Axis');
-ylabel('Amessageplitude');
 
-m_t=Amessage.*sin(2*pi*Fmessage*t); 
-subplot(4,1,2);
-plot(t,m_t);
-title('Message Signal');
-xlabel('Time Axis');
-ylabel('Amessageplitude');
+fc=100;
+fs=1000;
+f1=80;
+t=0:1/fs:((2/f1)-(1/fs));
+x1=0.4*cos(2*pi*f1*t)+0.5;
 
-for i=1:length(saw_signal)
-if (m_t(i)>=saw_signal(i))
-    pwm(i)=1; 
-else
-    pwm(i)=0;
-end
-end
-
-subplot(4,1,3);
-plot(t,pwm,'g');
-title('PWM');
-xlabel('Time Axis');
-ylabel('Amessageplitude');
-axis([0 1 0 1.1]);
+%modulation
+y1=modulate(x1,fc,fs,'ppm');
+subplot(311);
+plot(x1);
+axis([0 15 0 1]);
+title('modulating signal,f1=80,fs=1000')
+subplot(312);
+plot(y1);
+axis([0 250 -0.2 1.2]);
+title('PPM')
 
 %demodulation
-dt = pwm.*saw_signal;
-[b,a] = butter(4,0.01);
-retrieved = filter(b,a,dt);
-
-subplot(4,1,4);
-plot(t, retrieved);
-title('Demodulated Signal');
-xlabel('Time Axis');
-ylabel('Amessageplitude');
+x1_recov=demod(y1,fc,fs,'ppm');
+subplot(313);
+plot(x1_recov);
+title('time domain recovered, single tone,f1=80')
+axis([0 15 0 1]);
