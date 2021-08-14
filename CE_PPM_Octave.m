374c68231bcd32e21069c5d7e9717007a108bd01
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-08-14 17:30:03 +0530

    Update CE_PPM_Octave.m

--- CE_PPM_Octave.m
+++ CE_PPM_Octave.m
@@ -12,20 +12,20 @@ f2=20; % sawtooth frequency
 mssg=0.9*sin(2*pi*f1*t);
 subplot(4,1,1);
 plot(t,mssg);
-title('message signal')
+title('Message Signal (1904215)')
 
 %generation of sawtooth
 sa = sawtooth(2*pi*f2*t);
 subplot(4,1,2);
 plot(t,sa)
-title('Sawtooth waveform')
+title('Sawtooth Waveform (1904215)')
 
 %Pulse Width Modulation
 pwm=(mssg>sa);
 subplot(4,1,3);
 plot(t,pwm)
 axis([min(t),max(t),0,2])
-title('PWM Signal')
+title('Pulse Width Modulation (1904215)')
 
 %%PPM generation
 x1=~pwm; %inverter output
@@ -42,4 +42,4 @@ endwhile
 subplot(4,1,4)
 plot(0:0.0001:1.9999,ppm)
 axis([min(t),max(t),0,2])
-title('PPM modulated');
+title('Pulse Position Modulation (1904215)');
